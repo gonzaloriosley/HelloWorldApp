42f6e7cbeca6611dbbfbb1eb5e8b21d1348768d5
griosdp <gonzaloriosley@gmail.com>
2022-01-26 15:25:38 +0100

    Change in the string

--- .AL-Go/cloudDevEnv.ps1
+++ .AL-Go/cloudDevEnv.ps1
@@ -4,7 +4,7 @@
 # Recommended approach is to use as is or add a script (freddyk-devenv.ps1), which calls this script with the user specific parameters
 #
 Param(
-    [string] $environmentName = "",
+    [string] $environmentName = "Sandbox",
     [bool] $reuseExistingEnvironment
 )
 
